1c388c71c269173450a62d7cabbfed6aa004de70
DewashishTikas <tikasdewashish2208@gmail.com>
2026-02-25 10:50:36 +0530

    fixing bug

--- utlis/config.js
+++ utlis/config.js
@@ -5,14 +5,12 @@ function getConfig(isPackageJsonModified){
             "cd /home/ubuntu/Storage-App-Backend",
             "git pull",
             isPackageJsonModified ? "npm ci" : "",
-            "npm run test",
             "pm2 reload myFileSpace"
         ],
         "Storage-App-Frontend": [
             "cd /home/ubuntu/Storage-App-Frontend",
             "git pull",
             isPackageJsonModified ? "npm ci" : "",
-            "npm run test",
             "npm run build",
             "aws s3 sync ./dist s3://myfilespace-frontend",
             'aws cloudfront create-invalidation   --distribution-id E2UFBMZJ9YXU5V   --paths "/index.html"'
