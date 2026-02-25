07d263c091c64ff6ae969631f81c90801d830831
DewashishTikas <tikasdewashish2208@gmail.com>
2026-02-25 10:35:40 +0530

    fixing bug

--- utlis/pipeline.js
+++ utlis/pipeline.js
@@ -1,26 +1,26 @@
 import { spawn } from 'child_process'
 
-export function runPipeline({project,command}){
-   return new Promise((resolve,reject)=>{
-     const bashChildProcess = spawn("bash", [command])
-     bashChildProcess.stdout.on('data', (data) => {
-        process.stdout.write(data)
-        resolve({project,code,signal,err})
-     })
-     bashChildProcess.stderr.on('data', (data) => {
-        process.stderr.write(data)
-        reject({project,code,signal,err})
-     })
-     bashChildProcess.on("exit", async(code, signal,err) => {
-        if (code === 0) {
+export function runPipeline({ project, command }) {
+   return new Promise((resolve, reject) => {
+      const bashChildProcess = spawn("bash", [command])
+      bashChildProcess.stdout.on('data', (data) => {
+         process.stdout.write(data)
+         resolve({ project,data })
+      })
+      bashChildProcess.stderr.on('data', (data) => {
+         process.stderr.write(data)
+         reject({ project, err: data })
+      })
+      bashChildProcess.on("exit", async (code, signal, err) => {
+         if (code === 0) {
             console.log(`Script Ended with Code ${code}`)
-            resolve({project,code,signal,err})
-        } else {
+            resolve({ project, code, signal, err })
+         } else {
             console.log('Script failed ', code);
             console.log(signal);
             console.log(err);
-            reject({project,code,signal,err})
-        }
-     })
+            reject({ project, code, signal, err })
+         }
+      })
    })
 }
