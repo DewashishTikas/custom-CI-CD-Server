be7f356c461cab9a030b0bc206d7440a7c8ff6b6
DewashishTikas <tikasdewashish2208@gmail.com>
2026-03-13 15:30:01 +0530

    improve

--- utlis/config.js
+++ utlis/config.js
@@ -4,6 +4,7 @@ function getConfig(isPackageJsonModified) {
     const RETRY_DELAY = 5;
     return {
         "Storage-App-Backend": [
+            "pwd"
             `cd /home/ubuntu/Storage-App-Backend`,
             `mkdir release_${date}`,
             `cd release_${date}`,
