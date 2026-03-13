efdb4abeddc78f9ad90e4710c1253c838fd5492c
DewashishTikas <tikasdewashish2208@gmail.com>
2026-03-13 15:33:50 +0530

    improve

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.post("/webhook", async (req, res) => {
     res.sendStatus(200)
     const isPackageJsonModified = req.body.commits.some(({ modified }) => modified.includes("package.json"))
     const commands = getProjectCommands(req.body.repository.name, isPackageJsonModified, req.body.ref.includes("develop")).filter((command) => command)
-    let fullCommand ='set -e\n';
+    let fullCommand = 'set -e\n';
     for (const command of commands) {
-        fullCommand+=`${command}\n`
+        console.log(command);
+        fullCommand += `${command}\n`
     }
     try {
         await runPipeline({ project: `${req.body.repository.name}${req.body.ref.includes("develop") ? "-Test" : ""}`, fullCommand })
